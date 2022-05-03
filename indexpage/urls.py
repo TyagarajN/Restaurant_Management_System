fc660d017933f80df2a1115986d2adc46de0bc0c
TyagarajN <tyagarajnaik666@gmail.com>
2022-05-03 11:22:55 +0530

    routing pages

--- indexpage/urls.py
+++ indexpage/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+#url routing is done here
 urlpatterns = [
-    path('', views.index,name='index')
+    path('', views.index,name='index'),
+    path('contact/', views.contact,name='contact')
+    path('about/', views.about,name='about.html')
+    path('login/',views.login, name='login.html')
+    path('registration/',views.registration, name='regis.html')
 ]
--- indexpage/views.py
+++ indexpage/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def registration(request):
+    return render(request,'regis.html')
+
+def login(request):
+    return render(request, 'login.html')
