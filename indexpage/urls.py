d786100f67beb8fff774d0b51cc5753f5206e948
TyagarajN <tyagarajnaik666@gmail.com>
2022-05-04 01:09:43 +0530

    login and registration

--- indexpage/urls.py
+++ indexpage/urls.py
@@ -4,8 +4,8 @@ from . import views
 #url routing is done here
 urlpatterns = [
     path('', views.index,name='index'),
-    path('contact/', views.contact,name='contact')
-    path('about/', views.about,name='about.html')
-    path('login/',views.login, name='login.html')
+    path('contact/', views.contact,name='contact'),
+    path('about/', views.about,name='about.html'),
+    path('login/',views.login, name='login.html'),
     path('registration/',views.registration, name='regis.html')
 ]
