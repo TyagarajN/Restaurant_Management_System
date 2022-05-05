1d20136e98b342ebc0d81cd135638675a0bcbd9f
TyagarajN <tyagarajnaik666@gmail.com>
2022-05-06 02:49:13 +0530

    login

--- indexpage/views.py
+++ indexpage/views.py
@@ -45,4 +45,14 @@ def registration(request):
         return render(request,'regis.html')
 
 def login(request):
+    if request.method == 'POST':
+        username1 = request.POST['username']
+        password1 = request.POST['password']
+
+        user = auth.authenticate(username=username1, password=password1)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Please enter valid Credentials')
     return render(request, 'login.html')
